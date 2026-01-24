adfd5e70f690c909e274cd3a15ddfc8571fdd53c
Thomas Hunt <170183176+thomas0756@users.noreply.github.com>
2026-01-24 18:46:37 +0000

    Modify template

---
+++ src/main/java/com/amplicube/opalhud/OpalHUD.java
@@ -0,0 +1,16 @@
+package com.amplicube.opalhud;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OpalHUD implements ClientModInitializer {
+	public static final String MOD_ID = "opal-hud";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/com/amplicube/opalhud/OpalHUDDataGenerator.java
@@ -0,0 +1,11 @@
+package com.amplicube.opalhud;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OpalHUDDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
